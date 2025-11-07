7bc847a282f055320ce6a136839962634542ca26
Curtis Brown <curtisbrown2342@gmail.com>
2025-11-07 02:58:10 -0500

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/resume-optimizer',
+  assetPrefix: '/resume-optimizer/',
+}
+
+module.exports = nextConfig
